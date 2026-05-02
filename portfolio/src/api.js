97e08a85ab8e66df467ea299f7f91e32559a980a
EqsoftSreesh <eqsoftsreesh@gmail.com>
2026-05-02 15:50:22 +0530

    url updated

--- portfolio/src/api.js
+++ portfolio/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/api` : '/api',
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/api` 
+    : 'https://portfolio-maker-tkqn.onrender.com/api',
 });
 
 export default api;
